d6f46fc4799d8e49a0cd86e633b44d79a22ae159
Colt Daily <LeHaine@users.noreply.github.com>
2022-08-18 12:12:48 -0400

    art: update export_layers_and_tags.sh to allow file param

--- art/export_layers_and_tags.sh
+++ art/export_layers_and_tags.sh
@@ -5,11 +5,22 @@
 aseprite="D:/Program Files (x86)/SteamLibrary/steamapps/common/Aseprite/Aseprite.exe"
 fileName=""
 excludeFiles=()
-for file in ./ase/m_*.aseprite; do
+if [ -z "$1" ]; then
+  for file in ./ase/m_*.aseprite; do
+    fileName="${file##*/}"
+    fileName="${fileName%.*}"
+    fileName="${fileName:2}"
+    if [[ ! "${excludeFiles[*]}" =~ ${fileName} ]]; then
+      "$aseprite" -b --all-layers "$file" --save-as ./export_tiles/"${fileName}""_{layer}_{tag}"0.png
+    fi
+  done
+else
+  file="$1"
   fileName="${file##*/}"
   fileName="${fileName%.*}"
   fileName="${fileName:2}"
+  echo "Exporting ${file}"
   if [[ ! "${excludeFiles[*]}" =~ ${fileName} ]]; then
     "$aseprite" -b --all-layers "$file" --save-as ./export_tiles/"${fileName}""_{layer}_{tag}"0.png
   fi
-done
+fi
